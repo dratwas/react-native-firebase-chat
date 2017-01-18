ae9464a12c8a201d1be8ccf3fe329c258a723a60
WojtekSzafraniec <feelun132@gmail.com>
2017-01-18 15:44:29 +0100

    extension change.

--- src/containers/LoginScreen.js
+++ src/containers/LoginScreen.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-class LoginScreen extends React.Component {
+class LoginScreen extends Component {
   render() {
     return (
       <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
