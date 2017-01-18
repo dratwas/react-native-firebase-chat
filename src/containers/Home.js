1cfd84efc4f2dd1d9715db31f967987f493f7774
WojtekSzafraniec <feelun132@gmail.com>
2017-01-18 12:20:19 +0100

    Navigation.

---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { View, Text } from 'react-native';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+        <Text>Test screen!</Text>
+      </View>
+    );
+  }
+}
+
+export default Home;
